56595212cfcfb19e40155a2aee10dd8a35551998
lukebayes <lbayes@patternpark.com>
2011-03-07 19:37:09 -0800

    Updated examples

--- examples/view/main.js
+++ examples/view/main.js
@@ -1,10 +1,11 @@
 
-// Render the collection of things:
-renderWhatevers = ->
+# Render the collection of things:
+renderWhatevers = (cb) ->
   get '/whatever.json', (items) ->
     renderWhatever(item) for item in items
+    cb.call(this)
 
-// Render a single thing:
+# Render a single thing:
 renderWhatever = (item) ->
   div class: 'item', ->
     addText "Item Value" + item.value
@@ -21,7 +22,6 @@ html ->
   div class: 'foo', ->
     addText "Whatever"
 
-  renderWhatevers()
-
-  div class: 'footer'
+  renderWhatevers ->
+    div class: 'footer'
 
