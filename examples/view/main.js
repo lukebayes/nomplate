858a0be040eb8417c1e5fe8c4368fea67279fa2c
lukebayes <lbayes@patternpark.com>
2011-03-09 22:30:16 -0800

    Updated example template

--- examples/view/main.js
+++ examples/view/main.js
@@ -1,27 +1,19 @@
 
-# Render the collection of things:
-renderWhatevers = (cb) ->
-  get '/whatever.json', (items) ->
-    renderWhatever(item) for item in items
-    cb.call(this)
-
-# Render a single thing:
-renderWhatever = (item) ->
-  div class: 'item', ->
-    addText "Item Value" + item.value
-
 getTitle = ->
-  "Hello World"
+  'Hello World'
 
 html ->
   head ->
     title getTitle()
-    css "/stylesheets/application.css"
-    script "/javascripts/application.js"
+    css '/stylesheets/application.css'
+    script '/javascripts/application.js'
 
-  div class: 'foo', ->
-    addText "Whatever"
+  body ->
 
-  renderWhatevers ->
-    div class: 'footer'
+    div class: 'content', 'Whatever'
+
+    div class: 'footer', ->
+      a href: '/foo.html', 'Foo'
+      a href: '/bar.html', 'Bar'
+      
 
