976ac1676b541fa9f4c4f73a673259984bfe9c32
Luke Bayes <lukebayes@gmail.com>
2018-09-03 11:26:52 -0700

    Fixed typo

--- examples/express/views/main.js
+++ examples/express/views/main.js
@@ -3,7 +3,7 @@ const dom = require('../../../').dom; // require('nomplate').dom;
 /**
  * This is the default outer page template.
  *
- * You can decide where to render partials by calling the provided 'renderParial' handler.
+ * You can decide where to render partials by calling the provided 'renderPartial()' handler.
  */
 function main(options, renderPartial) {
   return dom.html(() => {
