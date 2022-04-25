9e3f18ccec8fb3e6ba2ca6f8d9077afb0a790dcc
Luke Bayes <lukebayes@gmail.com>
2022-04-25 19:25:49 -0400

    Working on varied id lengths

--- src/generate_id.js
+++ src/generate_id.js
@@ -8,7 +8,37 @@ for (let i = 0; i < 256; i += 1) {
   lut[i] = (i < 16 ? '0' : '') + (i).toString(16);
 }
 
-function generateId() {
+function generateId(opt_len) {
+  const len = opt_len || 13;
+  const halfLen = (len / 2) - 1;
+  const count = Math.ceil(len / 8);
+  const parts = new Array(len + 1);
+
+  let d = 0;
+  let pushed = 0;
+
+  for (let i = 0; i < count; i++) {
+    d = Math.random() * 0xffffffff | 0;
+    if (pushed++ < halfLen) {
+      parts.push(lut[d & 0xff]);
+    }
+    if (pushed++ < halfLen) {
+      parts.push(lut[d >> 8 & 0xff]);
+    }
+    if (pushed++ < halfLen) {
+      parts.push(lut[d >> 16 & 0xff]);
+    }
+    if (pushed++ < halfLen) {
+      parts.push(lut[d >> 24 & 0xff]);
+    }
+    if (pushed % 4 === 0 && pushed < halfLen) {
+      parts.push('-');
+    }
+  }
+
+  return parts.join('');
+
+  /*
   const d0 = Math.random() * 0xffffffff | 0;
   const d1 = Math.random() * 0xffffffff | 0;
 
@@ -20,6 +50,8 @@ function generateId() {
     lut[d1 >> 8 & 0xff];
 
   return id;
+  */
+
 }
 
 module.exports = generateId;
